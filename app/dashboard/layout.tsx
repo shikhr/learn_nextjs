5240fd88058dda4ee2d985bdb1b9208b5b92cdb4
Shikhar Mishra <shikharmishra730@gmail.com>
2024-01-16 01:38:54 +0530

    fix: database logic

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,4 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-// export const dynamic = 'force-dynamic';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
